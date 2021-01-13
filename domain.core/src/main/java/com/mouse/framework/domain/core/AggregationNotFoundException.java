39656873058884975bc14d265bfbbb4acade7355
xh.wang <xh.wang@datsch.com.cn>
2021-01-13 13:35:06 +0800

    init

---
+++ domain.core/src/main/java/com/mouse/framework/domain/core/AggregationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mouse.framework.domain.core;
+
+public class AggregationNotFoundException extends RuntimeException {
+    public AggregationNotFoundException(String aggregationName, String collectionName) {
+        super(String.format("Aggregation %s not found in collection %s", aggregationName, collectionName));
+    }
+}
