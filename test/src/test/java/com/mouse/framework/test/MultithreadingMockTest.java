7f615b26bfa2e802c51976a119fc011f0ec00751
xh.wang <xh.wang@datsch.com.cn>
2021-01-27 14:43:39 +0800

    Added multithreading test

---
+++ test/src/test/java/com/mouse/framework/test/MultithreadingMockTest.java
@@ -0,0 +1,49 @@
+package com.mouse.framework.test;
+
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpHeaders;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnableTestClient
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestClientTest.TestController.class)
+public class MultithreadingMockTest {
+    @Resource
+    private TestClient testClient;
+
+    private static Stream<Pair<Locale, String>> mockData() {
+        return Stream.of(Pair.of(Locale.SIMPLIFIED_CHINESE, "1"),
+                Pair.of(Locale.ENGLISH, "2"),
+                Pair.of(Locale.JAPANESE, "3"),
+                Pair.of(Locale.TRADITIONAL_CHINESE, "4"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("mockData")
+    @Execution(ExecutionMode.CONCURRENT)
+    void should_be_able_to_mock_in_multithreading(Pair<Locale, String> mockData) {
+        testClient.mockLanguage(mockData.getFirst());
+        testClient.mockToken(mockData.getSecond());
+        TestResponse response = testClient.get("/header");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAcceptLanguageAsLocales(Collections.singletonList(mockData.getFirst()));
+
+        String acceptLanguage = httpHeaders.getFirst(HttpHeaders.ACCEPT_LANGUAGE);
+
+        assertThat(response.getBody().strVal("$.accept-language")).isEqualTo(acceptLanguage);
+        assertThat(response.getBody().strVal("$.authorization")).isEqualTo(String.format("Bearer %s", mockData.getSecond()));
+    }
+}
