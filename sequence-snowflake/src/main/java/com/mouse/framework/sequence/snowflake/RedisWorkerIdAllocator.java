9127d6b7fc5b3d2b0d24bfb6e2d16149f8ebc54c
mouse9527 <m1689141525@gmail>
2021-01-30 03:01:31 +0800

    Refactored.

--- sequence-snowflake/src/main/java/com/mouse/framework/sequence/snowflake/RedisWorkerIdAllocator.java
+++ sequence-snowflake/src/main/java/com/mouse/framework/sequence/snowflake/RedisWorkerIdAllocator.java
@@ -2,22 +2,17 @@ package com.mouse.framework.sequence.snowflake;
 
 import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class RedisWorkerIdAllocator implements WorkerIdAllocator {
-    public static final int BASE = 1000;
     private final RedisTemplate<String, Long> redisTemplate;
     private final long maxEffectiveSeconds;
-    private final Timer timer;
     private final SnowFlakeProperties.WorkerIdProperties properties;
 
     public RedisWorkerIdAllocator(RedisTemplate<String, Long> redisTemplate, SnowFlakeProperties.WorkerIdProperties properties) {
         this.redisTemplate = redisTemplate;
         this.properties = properties;
         this.maxEffectiveSeconds = properties.getHeartbeatIntervalSeconds() * properties.getMaxFailedTimes();
-        timer = new Timer();
     }
 
     @Override
@@ -34,21 +29,6 @@ public class RedisWorkerIdAllocator implements WorkerIdAllocator {
         final String key = String.format("%s:%d", properties.getKeyPrefix(), id);
         redisTemplate.opsForValue().set(key, 1L);
         redisTemplate.expire(key, maxEffectiveSeconds, TimeUnit.SECONDS);
-        long interval = properties.getHeartbeatIntervalSeconds() * BASE;
-
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                Long count = redisTemplate.opsForValue().get(key);
-                if (count == null) {
-                    this.cancel();
-                    return;
-                }
-                redisTemplate.opsForValue().set(key, count + 1);
-                redisTemplate.expire(key, maxEffectiveSeconds, TimeUnit.SECONDS);
-            }
-        };
-        timer.schedule(task, interval, interval);
         return id;
     }
 }
--- sequence-snowflake/src/test/java/com/mouse/framework/sequence/snowflake/WorkerIdAllocatorTest.java
+++ sequence-snowflake/src/test/java/com/mouse/framework/sequence/snowflake/WorkerIdAllocatorTest.java
@@ -25,15 +25,12 @@ class WorkerIdAllocatorTest {
     }
 
     @Test
-    void should_be_able_to_allocate_worker_id() throws InterruptedException {
+    void should_be_able_to_allocate_worker_id() {
         long workerId = workerIdAllocator.allocate(MAX_WORKER_ID);
 
         assertThat(workerId).isEqualTo(0);
 
         assertThat(redisTemplate.opsForValue().get("WorkerId:0")).isEqualTo(1);
-        assertThat(redisTemplate.getExpire("WorkerId:0", TimeUnit.SECONDS)).isBetween(9L, 10L);
-        Thread.sleep(2000);
-        assertThat(redisTemplate.opsForValue().get("WorkerId:0")).isEqualTo(3);
         assertThat(redisTemplate.getExpire("WorkerId:0", TimeUnit.SECONDS)).isBetween(9L, 10L);
     }
 
@@ -47,5 +44,15 @@ class WorkerIdAllocatorTest {
 
         assertThat(redisTemplate.opsForValue().get("WorkerId:1")).isEqualTo(1);
         assertThat(redisTemplate.getExpire("WorkerId:1", TimeUnit.SECONDS)).isBetween(9L, 10L);
+
+        redisTemplate.opsForValue().set("WorkerId:2", 1L);
+        redisTemplate.opsForValue().set("WorkerId:3", 1L);
+        redisTemplate.opsForValue().set("WorkerId:5", 1L);
+
+        assertThat(workerIdAllocator.allocate(MAX_WORKER_ID)).isEqualTo(4);
+
+        assertThat(redisTemplate.opsForValue().get("WorkerId:4")).isEqualTo(1);
+        assertThat(redisTemplate.getExpire("WorkerId:4", TimeUnit.SECONDS)).isBetween(9L, 10L);
     }
+
 }
