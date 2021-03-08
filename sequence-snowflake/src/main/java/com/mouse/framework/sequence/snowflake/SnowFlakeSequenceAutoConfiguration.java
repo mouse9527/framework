46ca1ef732ebe87e37af79b59d0fe9324266ea71
mouse9527 <m1689141525@gmail>
2021-03-08 20:57:02 +0800

    Fixed bean name

--- sequence-snowflake/src/main/java/com/mouse/framework/sequence/snowflake/SnowFlakeSequenceAutoConfiguration.java
+++ sequence-snowflake/src/main/java/com/mouse/framework/sequence/snowflake/SnowFlakeSequenceAutoConfiguration.java
@@ -1,9 +1,7 @@
 package com.mouse.framework.sequence.snowflake;
 
 import com.mouse.framework.domain.core.SequenceService;
-import com.mouse.framework.domain.core.SequenceSetter;
 import lombok.Generated;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,23 +9,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Generated
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnBean(WorkerIdAllocator.class)
 @ConditionalOnMissingBean(SequenceService.class)
 @EnableConfigurationProperties(SnowFlakeProperties.class)
 public class SnowFlakeSequenceAutoConfiguration {
-    private final WorkerIdAllocator workerIdAllocator;
-    private final SnowFlakeProperties properties;
-
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public SnowFlakeSequenceAutoConfiguration(WorkerIdAllocator workerIdAllocator, SnowFlakeProperties properties) {
-        this.workerIdAllocator = workerIdAllocator;
-        this.properties = properties;
-    }
 
     @Bean
-    public SequenceService workerIdAllocator() {
-        SequenceService sequenceService = new SnowFlakeSequenceService(workerIdAllocator.allocate(), properties);
-        SequenceSetter.set(sequenceService);
-        return sequenceService;
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public SequenceService workerIdAllocator(WorkerIdAllocator workerIdAllocator, SnowFlakeProperties properties) {
+        return new SnowFlakeSequenceService(workerIdAllocator.allocate(), properties);
     }
 }
